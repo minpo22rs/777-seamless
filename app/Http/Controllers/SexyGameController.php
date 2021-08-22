0a383b8023435fc9e0b8d8c86740dd3c40231f89
chaleamwut <chaleamwut_b@boonrawd.co.th>
2021-08-22 21:08:04 +0700

    first commit

---
+++ app/Http/Controllers/SexyGameController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\SexyGame;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
+class SexyGameController extends Controller
+{
+    private $host = "https://tttint.onlinegames22.com";
+    private $certCode = "N19V3PqBl1QJtAyK85e";
+    private $agentId = "nasavg";
+    private $currencyCode = "THB";
+    private $language = "th";
+    private $betLimit = '{"SEXYBCRT":{"LIVE":{"limitId":[260901,260902,260903,260904,260905]}}} ';
+
+    public function login(Request $request, $username)
+    {
+        $user = User::where('username', $username)->first();
+        if ($user) {
+            try {
+                $client = new Client();
+                $res = $client->request('POST', $this->host . '/wallet/createMember', [
+                    'form_params' => [
+                        'cert' =>  $this->certCode,
+                        'agentId' =>  $this->agentId,
+                        'userId' =>  $this->username,
+                        'isMobileLogin' =>  false,
+                        'externalURL' =>  'https://www.google.com.tw/',
+                        'gameForbidden' =>  '{"JDBFISH":{"FH":["ALL"]}}',
+                        'gameType' =>  'SLOT',
+                        'platform' =>  'RT',
+                        'language' =>  $this->language,
+                        'betLimit' =>  $this->betLimit,
+                    ]
+                ]);
+                echo $res->getStatusCode();
+                // "200"
+                echo $res->getHeader('content-type')[0];
+                // 'application/json; charset=utf8'
+                echo $res->getBody();
+
+                // if (jsonObject.status == '0000') {
+                //     return response.redirect(`${jsonObject.url}`)
+                // } else if (jsonObject.status == '1002') {
+                //     let responsenewmember = await this.createMember(username);
+                //     console.log(responsenewmember);
+                //     if (responsenewmember.status != '0000') return responsenewmember; /// error create
+                //     if (!params.recall) {
+                //         params.recall = 1;
+                //         await this.login({ response, params });
+                //         return false;
+                //     }
+                // } else {
+                //     return jsonObject;
+                // }
+
+            } catch (BadResponseException $e) {
+                return response()->json([
+                    'message' => $e->getMessage()
+                ]);
+            }
+        } else {
+            $response = ["message" => 'Oops! The user does not exist'];
+            return response($response, 401);
+        }
+    }
+
+    public function getBalance(Request $request)
+    {
+        return [
+            'status' => '1000',
+            'desc' => 'Invalid user Id',
+        ];
+    }
+}
---
+++ app/Models/SexyGame.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SexyGame extends Model
+{
+    //
+    protected $table = 'sexy_transactions';
+}
